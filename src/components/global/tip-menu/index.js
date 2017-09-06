f869db08b1002c768e86d3f7901222a823a8ae89
刘蒙 <lemon.liu@eoitek.com>
2017-09-06 18:15:37 +0800

    添加事件移除操作

--- src/components/global/tip-menu/index.js
+++ src/components/global/tip-menu/index.js
@@ -6,6 +6,7 @@ import './index.scss';
 class TipMenuController {
   constructor($scope, $timeout) {
     this.services = { $scope, $timeout };
+    this.container = {};
   }
 
   scrollTo(id) {
@@ -13,23 +14,24 @@ class TipMenuController {
     document.getElementById(`${id}`).scrollIntoView({ behavior: 'smooth' });
   }
 
-  toggleSectionActive(container) {
-    const { $scope } = this.services;
-    const anchors = Array.from(document.getElementsByClassName('inpage-anchor'));
-    const visibleSection = anchors.find(x => {
-      const { top, bottom } = x.getBoundingClientRect();
-      return (top <= container.getBoundingClientRect().top && bottom > container.getBoundingClientRect().top);
-    });
-    this.activeSection.name = visibleSection ? visibleSection.id : '';
-    $scope.$apply();
-  };
-
   $onInit() {
-    // const container = angular.element(document.getElementById('scroll-container'));
-    const { $timeout } = this.services;
-    const container = document.getElementById('scroll-container');
-    container.addEventListener('scroll', () => { this.toggleSectionActive(container) }, { passive: true });
-    // this.toggleSectionActive(container);
+    const { $scope, $timeout } = this.services;
+    this.container = document.getElementById('scroll-container');
+    this.toggleSectionActive = () => {
+      const container = this.container;
+      const anchors = Array.from(document.getElementsByClassName('inpage-anchor'));
+      const visibleSection = anchors.find(x => {
+        const { top, bottom } = x.getBoundingClientRect();
+        return (top <= container.getBoundingClientRect().top && bottom > container.getBoundingClientRect().top);
+      });
+      this.activeSection.name = visibleSection ? visibleSection.id : '';
+      $scope.$apply();
+    }
+    this.container.addEventListener('scroll', this.toggleSectionActive, { passive: true });
+  }
+
+  $onDestroy() {
+    this.container.removeEventListener('scroll', this.toggleSectionActive, { passive: true });
   }
 
 }
